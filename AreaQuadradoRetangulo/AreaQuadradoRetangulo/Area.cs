6e74e89e790006dc66d2cee6f9872612ad900211
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2024-04-05 11:04:52 -0300

    Exercício Área do Quadrado ou Retângulo

---
+++ AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public int Altura {  get; set; }
+        public int Baseret {  get; set; }
+        
+
+        public int CalculaArea()
+        {
+            int altura = this.Altura;
+            int baseret = this.Baseret;
+            int area;
+            if (altura == baseret)
+            {
+                area = altura * altura;
+            }
+            else
+            {
+                area = altura * baseret;
+            }
+            return area;
+        }
+        public string CalculaArea2()
+        {
+            int altura = this.Altura;
+            int baseret = this.Baseret;
+            int area = 0;
+            string resultado = "";
+            if (altura == baseret)
+            {
+                area = altura * altura;
+                resultado = "Como a base e altura são iguais, ";
+                resultado += "será calculada a área de um QUADRADO.";
+                resultado += " A área é: " + area;
+            }
+            else
+            {
+                area = altura * baseret;
+                resultado = "Como a base e altura são diferentes, ";
+                resultado += "será calculada a área de um RETÂNGULO.";
+                resultado += " A área é: " + area;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcula a Área de um Quadrado ou Retângulo");
+            Area p = new Area();
+            Console.WriteLine("Digite o valor da Altura:");
+            p.Altura = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor da Base:");
+            p.Baseret = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("O valor da área será: ");
+            Console.WriteLine(p.CalculaArea());
+            //Console.WriteLine(p.CalculaArea2());
+        }
+    }
+}
